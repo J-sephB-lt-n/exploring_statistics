f2e557638db0120096187b37409ca592317a9054
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2020-04-24 09:16:30 +0200

    cleaning up

--- build_site.R
+++ build_site.R
@@ -3,6 +3,7 @@ library(tidyverse)
 setwd("~/exploring_statistics/exploring_statistics/")
 
 rmarkdown::render_site()
+# NOTE:  When a new post is added, you need to add it to the navbar dropdown in the website _site.yml
 
 
 ####################################################
@@ -143,102 +144,105 @@ navbar_creation_code <-
   
   
   
-  <div class="navbar navbar-inverse  navbar-fixed-top" role="navigation">
+<div class="navbar navbar-inverse  navbar-fixed-top" role="navigation">
   <div class="container">
-  <div class="navbar-header">
-  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
-  <span class="icon-bar"></span>
-  <span class="icon-bar"></span>
-  <span class="icon-bar"></span>
-  </button>
-  <a class="navbar-brand" href="index.html">Joseph Bolton</a>
-  </div>
-  <div id="navbar" class="navbar-collapse collapse">
-  <ul class="nav navbar-nav">
-  <li>
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.html">Joseph Bolton</a>
+    </div>
+    <div id="navbar" class="navbar-collapse collapse">
+      <ul class="nav navbar-nav">
+        <li>
   <a href="index.html"> Home</a>
-  </li>
-  <li>
+</li>
+<li>
   <a href="about.html"> About Me</a>
-  </li>
-  <li>
+</li>
+<li>
   <a href="future_content.html"> Future Content</a>
-  </li>
-  <li class="dropdown">
+</li>
+<li class="dropdown">
   <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-  Posts
-
-<span class="caret"></span>
+     Posts
+     
+    <span class="caret"></span>
   </a>
   <ul class="dropdown-menu" role="menu">
-  <li class="dropdown-header">Posts</li>
-  <li>
-  <a href="recommenders_part1_vectors.html">Recommenders Part I: Representing Customers &amp; Items as Vectors</a>
-  </li>
-  <li>
-  <a href="nice_R_visualisations.html">Beautiful Lesser-Known Visualisations in R</a>
-  </li>
-  <li>
-  <a href="gbm_from_scratch_8020.html">Building a GBM from Scratch</a>
-  </li>
-  <li>
-  <a href="good_books_and_papers.html">My Favourite Books, Papers and other Resources</a>
-  </li>
-  <li>
-  <a href="neural_net_from_scratch.html">Deep-Learning from Scratch</a>
-  </li>
-  <li>
-  <a href="PAMS_and_SILHOUETTE_by_hand.html">Clustering: PAM k-Medoids, CLARA &amp; Silhouette Values</a>
-  </li>
-  <li>
-  <a href="intuition_for_CRVTW_uplift_model.html">CRVTW Revenue Uplift Model</a>
-  </li>
-  <li>
-  <a href="data_tree_checkout.html">Data.trees</a>
-  </li>
-  <li>
-  <a href="constrOptim.html">Constrained Optimisation in R</a>
-  </li>
-  <li>
-  <a href="OLS_theory.html">Least Squares Linear Model Theory</a>
-  </li>
+    <li class="dropdown-header">Posts</li>
+    <li>
+      <a href="recommenders_part1_vectors.html">Recommenders Part I: Representing Customers &amp; Items as Vectors</a>
+    </li>
+    <li>
+      <a href="nice_R_visualisations.html">Beautiful Lesser-Known Visualisations in R</a>
+    </li>
+    <li>
+      <a href="gbm_from_scratch_8020.html">Building a GBM from Scratch</a>
+    </li>
+    <li>
+      <a href="good_books_and_papers.html">My Favourite Books, Papers and other Resources</a>
+    </li>
+    <li>
+      <a href="neural_net_from_scratch.html">Deep-Learning from Scratch</a>
+    </li>
+    <li>
+      <a href="visual_intro_to_splines.html">Regression Splines</a>
+    </li>
+    <li>
+      <a href="PAMS_and_SILHOUETTE_by_hand.html">Clustering: PAM k-Medoids, CLARA &amp; Silhouette Values</a>
+    </li>
+    <li>
+      <a href="intuition_for_CRVTW_uplift_model.html">CRVTW Revenue Uplift Model</a>
+    </li>
+    <li>
+      <a href="data_tree_checkout.html">Data.trees</a>
+    </li>
+    <li>
+      <a href="constrOptim.html">Constrained Optimisation in R</a>
+    </li>
+    <li>
+      <a href="OLS_theory.html">Least Squares Linear Model Theory</a>
+    </li>
   </ul>
-  </li>
-  </ul>
-  <ul class="nav navbar-nav navbar-right">
-  <li>
+</li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li>
   <a href="mailto:joseph.jazz.bolton@gmail.com">
-  <span class="fa fa-envelope"></span>
-  
+    <span class="fa fa-envelope"></span>
+     
   </a>
-  </li>
-  <li>
+</li>
+<li>
   <a href="https://www.linkedin.com/in/joseph-bolton-a02653171/">
-  <span class="fa fa-linkedin"></span>
-  
+    <span class="fa fa-linkedin"></span>
+     
   </a>
-  </li>
-  </ul>
-  </div><!--/.nav-collapse -->
+</li>
+      </ul>
+    </div><!--/.nav-collapse -->
   </div><!--/.container -->
-  </div><!--/.navbar -->
+</div><!--/.navbar -->
   
 '
  
 # specify name(s) of posts to insert navbar into (as a character vector): 
 post_names_to_insert_navbar <- ""  
-   # c( "constrOptim.html", 
-   #    "data_tree_checkout.html", 
-   #    "gbm_from_scratch_8020.html", 
-   #    "good_books_and_papers.html", 
-   #    "intuition_for_CRVTW_uplift_model.html", 
-   #    "neural_net_from_scratch.html", 
-   #    "nice_R_visualisations.html", 
-   #    "OLS_test.html", 
-   #    "OLS_theory.html", 
-   #    "PAMS_and_SILHOUETTE_by_hand.html", 
-   #    "recommenders_part1_vectors.html"
-   #  )
+   c( "visual_intro_to_splines.html",
+      "constrOptim.html",
+      "data_tree_checkout.html",
+      "gbm_from_scratch_8020.html",
+      "good_books_and_papers.html",
+      "intuition_for_CRVTW_uplift_model.html",
+      "neural_net_from_scratch.html",
+      "nice_R_visualisations.html",
+      "OLS_theory.html",
+      "PAMS_and_SILHOUETTE_by_hand.html",
+      "recommenders_part1_vectors.html"
+    )
 
 for( post_i in post_names_to_insert_navbar ){
   
